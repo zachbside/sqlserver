5b4bf99057464ea26b19e0cb5e88676d0c695a70
zachbside <53321834+zachbside@users.noreply.github.com>
2019-07-26 11:15:20 -0400

    Create CreateUser.sql

---
+++ CreateUser.sql
@@ -0,0 +1,14 @@
+USE [master]
+GO
+
+CREATE LOGIN [tester] WITH PASSWORD=N'test', DEFAULT_DATABASE=[test], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+GRANT VIEW SERVER STATE TO tester
+GO
+
+USE --enter database name here
+GO
+CREATE USER [tester] FOR LOGIN [tester] WITH DEFAULT_SCHEMA=[tester]
+GRANT EXECUTE ON [dbo].[Gathermetrics] TO [tester]
+GO
